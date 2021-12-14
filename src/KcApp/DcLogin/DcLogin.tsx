7c4d9cd8cf485b2b1c3e1522137a7e996d1dc075
Ron Milich <RMilich@Digitalclues.com>
2021-12-14 12:01:12 +0200

    updated DcLogin form properties

--- src/KcApp/DcLogin/DcLogin.tsx
+++ src/KcApp/DcLogin/DcLogin.tsx
@@ -1,11 +1,12 @@
 import { memo, useState } from "react";
-import type { KcProps } from "keycloakify";
+import { KcProps } from "keycloakify";
+import { Template } from "keycloakify/lib/components/Template";
 import type { KcContext } from "../kcContext";
 import styles from "./DcLogin.module.scss";
 import ParticlesWrapper from "./ParticlesWrapper";
 import { FcKey } from "react-icons/fc";
 
-type DcLogin = Extract<KcContext, { pageId: "dc-login.ftl" }>;
+type DcLogin = Extract<KcContext, { pageId: "login.ftl" }>;
 
 export const DcLoginTheme = memo(
   ({ kcContext, ...props }: { kcContext: DcLogin } & KcProps) => {
@@ -21,6 +22,77 @@ export const DcLoginTheme = memo(
           e.target.value === "" ? `Please enter a ${e.target.id}` : "",
       }));
     };
+
+    console.log({ url });
+
+    // return (
+    //   <Template
+    //     showUsernameNode={false}
+    //     infoNode={false}
+    //     headerNode={false}
+    //     doFetchDefaultThemeResources={false}
+    //     {...{ kcContext, ...props }}
+    //     formNode={
+    //       <div className={styles.login_page_container}>
+    //         <div className={styles.login_window_container}>
+    //           <ParticlesWrapper />
+    //           <h3 className={styles.slogen_phrase}>
+    //             <FcKey />
+    //             <strong>Welcome</strong>, to start investigating, please login
+    //           </h3>
+    //           <form
+    //             action={url.loginAction}
+    //             method="post"
+    //             className={styles.login_form}
+    //           >
+    //             <div className={styles.form_control}>
+    //               <input
+    //                 className={styles.login_input}
+    //                 placeholder="Username"
+    //                 type="text"
+    //                 id="username"
+    //                 value={username}
+    //                 onChange={(e) => {
+    //                   setUsername(e.target.value);
+    //                   changeValueHandler(e);
+    //                 }}
+    //                 name="username"
+    //               />
+    //               {errors.username && (
+    //                 <span className={styles.error}>{errors.username}</span>
+    //               )}
+    //             </div>
+
+    //             <div className={styles.form_control}>
+    //               <input
+    //                 className={styles.login_input}
+    //                 placeholder="Password"
+    //                 type="password"
+    //                 id="password"
+    //                 value={password}
+    //                 onChange={(e) => {
+    //                   setPassword(e.target.value);
+    //                   changeValueHandler(e);
+    //                 }}
+    //                 name="password"
+    //               />
+    //               {errors.password && (
+    //                 <span className={styles.error}>{errors.password}</span>
+    //               )}
+    //             </div>
+    //             <button
+    //               type="submit"
+    //               className={styles.login_button}
+    //               disabled={username === "" || password === ""}
+    //             >
+    //               Login
+    //             </button>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     }
+    //   />
+    // );
 
     return (
       <div className={styles.login_page_container}>
@@ -46,6 +118,7 @@ export const DcLoginTheme = memo(
                   setUsername(e.target.value);
                   changeValueHandler(e);
                 }}
+                name="username"
               />
               {errors.username && (
                 <span className={styles.error}>{errors.username}</span>
@@ -63,6 +136,7 @@ export const DcLoginTheme = memo(
                   setPassword(e.target.value);
                   changeValueHandler(e);
                 }}
+                name="password"
               />
               {errors.password && (
                 <span className={styles.error}>{errors.password}</span>
